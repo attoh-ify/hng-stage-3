04df971c136a005206846f63d1efc754184b98a9
Attoh Ifeanyi <114439637+attoh-ify@users.noreply.github.com>
2025-11-02 20:35:03 +0100

    Commit

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,7 @@
 import { Mastra } from "@mastra/core";
 import { createLogger } from "@mastra/core/logger";
 import { LibSQLStore } from "@mastra/libsql";
-import { backendMentorAgent } from "./agents/index.js";
+import { backendMentorAgent } from "./agents/backendMentorAgent.js";
 import { a2aAgentRoute } from "./a2a/router.js";
 
 export const mastra = new Mastra({
