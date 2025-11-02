1ea988c7fadb953b513d70c516c587f2caaa5723
Attoh Ifeanyi <114439637+attoh-ify@users.noreply.github.com>
2025-11-02 13:14:46 +0100

    Commit

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,6 +2,7 @@ import { Mastra } from "@mastra/core";
 import { createLogger } from "@mastra/core/logger";
 
 import agents from "./agents/index.js";
+import { a2aAgentRoute } from "../a2a/router.js";
 
 const mastra = new Mastra({
     agents,
@@ -9,6 +10,16 @@ const mastra = new Mastra({
         name: "BackendMentorAgent",
         level: "info",
     }),
+    observability: {
+    default: { enabled: true },
+  },
+  server: {
+    build: {
+      openAPIDocs: true,
+      swaggerUI: true,
+    },
+    apiRoutes: [a2aAgentRoute]
+  }
 });
 
 export { mastra };
